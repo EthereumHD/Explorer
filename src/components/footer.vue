4ac00fd0679bb7ae56b461931517f372a26960a8
EthereumHD <lucas.libra@outlook.com>
2020-02-26 16:53:09 +0800

    init version

---
+++ src/components/footer.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="footer">
+        <div class="footer-info">@{{ tYear }} Powered by EHD</div>
+    </div>
+</template>
+<script>
+export default {
+    name: "footBottom",
+    data() {
+        return {
+            tYear: ""
+        };
+    },
+    mounted() {
+        this.doHandleYear();
+    },
+    methods: {
+        doHandleYear() {
+            let myDate = new Date();
+            this.tYear = myDate.getFullYear();
+        }
+    }
+};
+</script>
+<style scoped>
+.footer {
+    padding-top: 30px;
+    width: 100%;
+    line-height: 60px;
+    color: #9b9b9b;
+    font-size: 12px;
+    clear: both;
+    text-align: center;
+}
+</style>
